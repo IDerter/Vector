34682cbe05d53b20b40db467dbecf868dee7b9a7
Daniil Turchaninov <indiegames1@mail.ru>
2022-03-22 17:04:59 +0300

    Bug Fixed

--- Vector2DZ/Vector2DZ.cpp
+++ Vector2DZ/Vector2DZ.cpp
@@ -12,7 +12,7 @@ int main()
     v1.SetVec(x1, y1);
     v2.SetVec(x2, y2);
     std::cout << "Scal proizved: "<<v1.Scal2V(v2)<<std::endl;
-    std::cout << "Vec proizved: " << v1.VectYmn2V(v2) << std::endl;
+    std::cout << "Vec proizved: " <<"0 0 "<< v1.VectYmn2V(v2) << std::endl;
     std::cout << "Vec 1 length: " << v1.LengthVec() << std::endl;
     std::cout << "Vec 2 length: " << v2.LengthVec()<<std::endl;
 }
